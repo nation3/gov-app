564016401a63e5151a09a9b3d2223daa0fbe45d6
Luis Cuende <me@luisivan.net>
2022-07-22 16:12:19 +0100

    feat(ui): It works!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,61 @@
 import type { NextPage } from 'next'
 import Form from '@rjsf/core'
-import styles from '../styles/Home.module.css'
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
+import json from 'react-syntax-highlighter/dist/cjs/languages/hljs/json'
+import { tomorrow } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
 
-import specSchema from '../../governance/validator/N3GOV-v1.json'
+import { v1 } from '@nation3/gov-specs'
 import uiSchema from '../uiSchema.json'
 
-const schema = {
-  title: 'Todo',
-  type: 'object',
-  required: ['title'],
-  properties: {
-    title: { type: 'string', title: 'Title', default: 'A new task' },
-    done: { type: 'boolean', title: 'Done?', default: false },
-  },
-}
+import { useState, useEffect } from 'react'
+
+SyntaxHighlighter.registerLanguage('json', json)
 
 const Home: NextPage = () => {
+  const [proposalDraft, setProposalDraft] = useState({})
+
+  useEffect(() => {
+    const savedDraft = JSON.parse(
+      localStorage.getItem('proposal-draft') || '{}'
+    )
+    savedDraft?.spec && setProposalDraft(savedDraft)
+  }, [])
+  useEffect(() => {
+    localStorage.setItem('proposal-draft', JSON.stringify(proposalDraft))
+  }, [proposalDraft])
+
   return (
-    <div className={styles.container}>
-      <Form schema={specSchema} uiSchema={uiSchema} />
+    <div className="hero">
+      <div className="hero-content">
+        <div className="card w-full xl:max-w-5xl shadow-xl">
+          <div className="card-body">
+            <div className="card-title text-2xl">
+              Create a governance proposal
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+              <Form
+                schema={v1}
+                uiSchema={uiSchema}
+                formData={proposalDraft}
+                onChange={(e) => setProposalDraft(e.formData)}
+                action="#"
+              />
+              <div>
+                <div className="card-title text-lg mt-8 mb-4">
+                  Proposal output
+                </div>
+                <SyntaxHighlighter language="javascript" style={tomorrow}>
+                  {JSON.stringify(proposalDraft, null, 2)}
+                </SyntaxHighlighter>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
+
+// https://api.gitrows.com/@github/nation3/governance/proposals/N3GOV-1.json
 
 export default Home
