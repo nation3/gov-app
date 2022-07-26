e163dae29fd256033b295ded7e1d2bf136e077c0
Luis Cuende <me@luisivan.net>
2022-07-26 18:49:38 +0100

    fix(ui): Responsive fix

--- pages/proposals/[id].tsx
+++ pages/proposals/[id].tsx
@@ -158,7 +158,7 @@ const Proposal: NextPage = ({ proposal }: any) => {
                 #{proposal.id} {proposal.discussionMetadata.title}
               </h2>
 
-              <div className="grid grid-cols-2 gap-4">
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
                 <div>
                   {/*@ts-ignore*/}
                   {proposalComponents(proposal)[proposal.content.kind]}
